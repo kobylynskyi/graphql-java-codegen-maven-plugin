5e9fcba732ec6b80afb8e7dda75d3eba2fb101d2
Bogdan Kobylynskyi <92bogdan@gmail.com>
2019-09-06 21:30:17 -0500

    Add GraphqlCodegenMojo

---
+++ src/main/java/io/github/kobylynskyi/graphql/codegen/GraphqlCodegenMojo.java
@@ -0,0 +1,121 @@
+package io.github.kobylynskyi.graphql.codegen;
+
+import com.kobylynskyi.graphql.codegen.GraphqlCodegen;
+import com.kobylynskyi.graphql.codegen.model.MappingConfig;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+@Mojo(name = "graphqlCodegen")
+public class GraphqlCodegenMojo extends AbstractMojo {
+
+    @Parameter(required = true)
+    private String[] graphqlSchemaPaths;
+    @Parameter(required = true)
+    private File outputDir;
+    @Parameter
+    private Properties customTypesMapping;
+    @Parameter
+    private String packageName;
+    @Parameter
+    private String apiPackageName;
+    @Parameter
+    private String modelPackageName;
+    @Parameter
+    private String modelNamePrefix;
+    @Parameter
+    private String modelNameSuffix;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        MappingConfig mappingConfig = new MappingConfig();
+        mappingConfig.setPackageName(packageName);
+        mappingConfig.setCustomTypesMapping(convertToMap(customTypesMapping));
+        mappingConfig.setModelNamePrefix(modelNamePrefix);
+        mappingConfig.setModelNameSuffix(modelNameSuffix);
+        mappingConfig.setApiPackageName(apiPackageName);
+        mappingConfig.setModelPackageName(modelPackageName);
+        try {
+            new GraphqlCodegen(Arrays.asList(graphqlSchemaPaths), outputDir, mappingConfig).generate();
+        } catch (Exception e) {
+            throw new MojoExecutionException("Unable to generate code", e);
+        }
+    }
+
+    private static Map<String, String> convertToMap(Properties properties) {
+        Map<String, String> map = new HashMap<>();
+        properties.forEach((key, value) -> map.put((String) key, (String) value));
+        return map;
+    }
+
+    public String[] getGraphqlSchemaPaths() {
+        return graphqlSchemaPaths;
+    }
+
+    public void setGraphqlSchemaPaths(String[] graphqlSchemaPaths) {
+        this.graphqlSchemaPaths = graphqlSchemaPaths;
+    }
+
+    public File getOutputDir() {
+        return outputDir;
+    }
+
+    public void setOutputDir(File outputDir) {
+        this.outputDir = outputDir;
+    }
+
+    public Properties getCustomTypesMapping() {
+        return customTypesMapping;
+    }
+
+    public void setCustomTypesMapping(Properties customTypesMapping) {
+        this.customTypesMapping = customTypesMapping;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getApiPackageName() {
+        return apiPackageName;
+    }
+
+    public void setApiPackageName(String apiPackageName) {
+        this.apiPackageName = apiPackageName;
+    }
+
+    public String getModelPackageName() {
+        return modelPackageName;
+    }
+
+    public void setModelPackageName(String modelPackageName) {
+        this.modelPackageName = modelPackageName;
+    }
+
+    public String getModelNamePrefix() {
+        return modelNamePrefix;
+    }
+
+    public void setModelNamePrefix(String modelNamePrefix) {
+        this.modelNamePrefix = modelNamePrefix;
+    }
+
+    public String getModelNameSuffix() {
+        return modelNameSuffix;
+    }
+
+    public void setModelNameSuffix(String modelNameSuffix) {
+        this.modelNameSuffix = modelNameSuffix;
+    }
+}
